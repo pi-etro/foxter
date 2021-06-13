50d04ed59dd23b47f0b11a4998490e729e502fe0
Pietro <pietro.dcg@gmail.com>
2021-06-13 07:16:49 -0300

    Initial Server-Peer interface

--- Peer.java
+++ Peer.java
@@ -4,51 +4,97 @@ import java.net.DatagramPacket;
 import java.net.InetAddress;
 import Mensagem.*;
 
-class Peer{
+//@SuppressWarnings("resource") // removes never closer warnings
 
+public class Peer {
 
+	public static void main(String args[]) throws Exception {
 
-    public static void main(String args[]) throws Exception{
+		InetAddress serverAddress = InetAddress.getByName("127.0.0.1");
+		int serverPort = 10098;
 
-        InetAddress serverAddress = InetAddress.getByName("127.0.0.1");
-        int serverPort = 10098;
+		// server address input
+		/*
+		 * Scanner input = new Scanner(System.in);
+		 * System.out.print("Entre com o endereço IP deste peer: "); InetAddress
+		 * peerAddress = InetAddress.getByName(input.nextLine());
+		 * System.out.print("Entre com a porta deste peer: "); int peerPort =
+		 * Integer.parseInt(input.nextLine()); input.close();
+		 */
 
-        ServerIn sin = new ServerIn();
-        ServerOut sout = new ServerOut();
-        sin.start();
-        sout.start();
+		InetAddress peerAddress = InetAddress.getByName("127.0.0.1"); // temporario
+		int peerPort = 55000; // temporario
 
-        while (true){
-        Scanner input = new Scanner(System.in);
-        String mensagem = input.nextLine();
-        DatagramSocket clientSocket = new DatagramSocket(); // SO da uma porta automaticamente
-        byte[] sendData = new byte[1024];
-        sendData = mensagem.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
-        clientSocket.send(sendPacket);
+		ServerHandler server = new ServerHandler();
+		server.start();
 
-		byte[] recBuffer = new byte[1024];
-		DatagramPacket recPacket = new DatagramPacket(recBuffer, recBuffer.length);
-		clientSocket.receive(recPacket); // BLOCKING
+		Scanner in = new Scanner(System.in);
+		while (true) {
+			System.out.print("MENU:\n1 JOIN\n2 SEARCH\n3 DOWNLOAD\nEntre com a opção: ");
+			int option = in.nextInt();
+			switch(option){
+				case 1:
+					// JOIN
+					break;
+				case 2:
+					// SEARCH
+					break;
+				case 3:
+					// DOWNLOAD
+					break;
+				default:
+					System.out.println("Opção inválida!");
+			}
+		}
 
-		String info = new String(recPacket.getData(), recPacket.getOffset(), recPacket.getLength());
-		System.out.println(info);
-    }
-
-
-
-    }
+	}
 }
 
-class ServerIn extends Thread{
-    public void run(){
-        System.out.println("thread ClientIn");
-    }
+class ServerHandler extends Thread {
+
+	private DatagramPacket recPacket;
+
+	public void run() {
+		// se for pacote ALIVE ativa flag para responder
+		// caso contrario repassa pacote para o menu
+		try {
+			DatagramSocket clientSocket = new DatagramSocket(10098);
+
+			// awaits serber contact
+			while (true) {
+				byte[] recBuffer = new byte[1024];
+				DatagramPacket recPacket = new DatagramPacket(recBuffer, recBuffer.length);
+				clientSocket.receive(recPacket); // blocking
+				String message = new String(recPacket.getData(), recPacket.getOffset(), recPacket.getLength());
+				if (message.equals("ALIVE")) {
+					AliveHandler answer = new AliveHandler();
+					answer.start();
+				} else {
+					setPacket(recPacket);
+				}
+			}
+		} catch (Exception e) {
+
+		}
+	}
+
+	public void setPacket(DatagramPacket packet) {
+		this.recPacket = packet;
+	}
+
+	public DatagramPacket getPacket() {
+		return this.recPacket;
+	}
 }
 
-class ServerOut extends Thread{
-    public void run(){
-        System.out.println("thread ClientOut");
+class AliveHandler extends Thread {
+	public void run() {
+		System.out.println("thread AliveHandler");
+	}
+}
 
-    }
+class PeerHandler extends Thread {
+	public void run() {
+		System.out.println("thread PeerHandler");
+	}
 }
--- Servidor.java
+++ Servidor.java
@@ -1,16 +1,7 @@
+
 /*
 Inicializacao:
 Recebe entrada pelo teclado do endereço IP, porta default 10098
-Requisicoes:
-JOIN: recebe nome dos arquivos do peer, reponde JOIN_OK
-
-LEAVE: remove informacoes do peer, responde LEAVE_OK
-
-SEARCH: recebe nome do arquivo, responde com lista de peers que possui arquivo
-
-UPDATE: recebe de um peer que baixou o arquivo, atualiza info deste peer, responde UPDATE_OK
-
-ALIVE: enviado a cada 30 segundo aos peers, recebe ALIVE_OK, caso não, remove informações do peer
 */
 
 import java.util.Scanner;
@@ -19,63 +10,68 @@ import java.net.DatagramPacket;
 import java.net.InetAddress;
 import Mensagem.*;
 
-public class Servidor{
+//@SuppressWarnings("resource") // removes never closer warnings
 
-    public static String serverAddress;
-    public static int serverPort = 10098;
+public class Servidor {
 
-    public static void main(String args[])throws Exception{
+	public static String serverAddress;
+	public static int serverPort = 10098;
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Entre com o endereço IP deste servidor: ");
-        Servidor.serverAddress = input.nextLine();
-        input.close();
-		Servidor.serverAddress = "127.0.0.1"; //temporario
+	public static void main(String args[]) throws Exception {
 
-        ClientIn cin = new ClientIn();
-        ClientOut cout = new ClientOut();
-        cin.start();
-        cout.start();
+		// address input
+		/*
+		Scanner input = new Scanner(System.in);
+		System.out.print("Entre com o endereço IP do servidor: ");
+		Servidor.serverAddress = input.nextLine();
+		input.close();
+		Servidor.serverAddress = "127.0.0.1"; // temporario
+		*/
 
-		DatagramSocket serverSocket = new DatagramSocket(10098);
-		while(true){
+		DatagramSocket serverSocket = new DatagramSocket(serverPort);
+
+		// awaits peers contact
+		while (true) {
 			byte[] recBuffer = new byte[1024];
 			DatagramPacket recPacket = new DatagramPacket(recBuffer, recBuffer.length);
-			serverSocket.receive(recPacket); // BLOCKING
-			String info = new String(recPacket.getData(), recPacket.getOffset(), recPacket.getLength());
-			System.out.println(info);
-
-			byte[] sendData = new byte[1024];
-			sendData = "resposta do servidor".getBytes();
-			InetAddress clientAddress = recPacket.getAddress();
-			int clientPort = recPacket.getPort();
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
-	        serverSocket.send(sendPacket);
+			serverSocket.receive(recPacket); // blocking
+			ClientHandler peer = new ClientHandler(recPacket);
+			peer.start();
 		}
-
-    }
+	}
 }
 
-class ClientIn extends Thread{
-    public void run(){
-        System.out.println("thread ClientIn");
-    }
+class ClientHandler extends Thread {
+
+	public InetAddress clientAddress;
+	public int clientPort;
+	public String clientMessage;
+
+	public ClientHandler(DatagramPacket recPacket) {
+		// unpack datagram
+		this.clientAddress = recPacket.getAddress();
+		this.clientPort = recPacket.getPort();
+		this.clientMessage = new String(recPacket.getData(), recPacket.getOffset(), recPacket.getLength());
+	}
+
+	public void run() {
+		System.out.println(this.clientAddress);
+		System.out.println(this.clientPort);
+		System.out.println(this.clientMessage);
+	}
 }
 
-class ClientOut extends Thread{
-    public void run(){
-        System.out.println("thread ClientOut");
-    }
-}
-
-class AliveSend extends Thread{
-    public void run(){
-        System.out.println("thread to check if peers are alive");
-    }
-}
-
-class AliveReceive extends Thread{
-    public void run(){
-        System.out.println("thread to check if peers are alive");
-    }
-}
+/*
+ * Requisicoes: JOIN: recebe nome dos arquivos do peer, reponde JOIN_OK
+ *
+ * LEAVE: remove informacoes do peer, responde LEAVE_OK
+ *
+ * SEARCH: recebe nome do arquivo, responde com lista de peers que possui
+ * arquivo
+ *
+ * UPDATE: recebe de um peer que baixou o arquivo, atualiza info deste peer,
+ * responde UPDATE_OK
+ *
+ * ALIVE: enviado a cada 30 segundo aos peers, recebe ALIVE_OK, caso não, remove
+ * informações do peer
+ */
